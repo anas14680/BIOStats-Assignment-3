b0fba21159951ef685966d507e5986f0cb23f09f
anas14680 <mohammadanas109@gmail.com>
2022-02-24 00:43:12 -0500

    Modify EHR

--- Ehr_OOP.py
+++ Ehr_OOP.py
@@ -14,23 +14,12 @@ class Patient:
         self.DOB = None
         self.race = None
 
-    def extract_info(self, filename):
+    def __repr__(self):
+        """Patient Class Representation."""
+        return self.patientID + " Class"
+
+    def store_info(self, patientinfo):
         """Extract info of a patient ID from text file."""
-        with open(filename, encoding="UTF-8-sig") as file:
-            counter = 0
-            for line in file:
-                counter += 1
-                line = line.split("\t")
-                line[-1] = line[-1][:-1]
-                if counter == 1:
-                    key = line
-
-                else:
-                    if line[0] == self.PatientID:
-                        val = line
-
-        patientinfo = dict(zip(key, val))
-
         self.gender = patientinfo["PatientGender"]
         self.DOB = datetime.strptime(
             patientinfo["PatientDateOfBirth"], "%Y-%m-%d %H:%M:%S.%f"
@@ -44,31 +33,65 @@ class Patient:
         return int((self.DOB - today).days / 365.25)
 
 
-class lab:
+class Lab:
     """Store Lab data."""
 
     def __init__(self, Labname):
         """Instantiate Lab Class."""
         self.labname = Labname
-        self.units = None
-        self.value = None
+        self.units = []
+        self.value = []
+        self.patientID = []
 
-    def extract_lab_info(self, filename):
+    def __repr__(self):
+        """Lab Class Represenatation."""
+        return self.labname + " Class"
+
+    def store_info(self, lab_info):
         """Extract Lab info."""
-        Units = []
-        Values = []
-        with open(filename, encoding="UTF-8-sig") as file:
-            counter = 0
-            for line in file:
-                counter += 1
-                line = line.split("\t")
-                if counter == 1:
-                    unitindex = line.index("LabValue")
-                    valueindex = line.index("LabUnits")
-                else:
-                    if line[0] == self.labname:
-                        Units.append(line[unitindex])
-                        Values.append(line[valueindex])
+        self.value.append(lab_info["LabValue"])
+        self.units.append(lab_info["LabUnits"])
+        self.patientID.append(lab_info["PatientID"])
 
-        self.units = Units
-        self.value = Values
+
+# redefine the function parse data that returns a class
+
+
+def parse_data(filename, pt_lb):
+    """Parse patient or lab data."""
+    assert pt_lb in [
+        "patient",
+        "lab",
+    ], "pt_lb takes patient or lab as operation"
+
+    data_dict = {}
+
+    with open(filename, encoding="UTF-8-sig") as file:
+        counter = 0
+
+        for line in file:
+            counter += 1
+            line = line.split("\t")
+            line[-1] = line[-1][:-1]
+            if counter == 1:
+                key = line
+            else:
+                val = line
+
+                information_dict = dict(zip(key, val))
+
+                if pt_lb == "patient":
+                    patient_class = Patient(information_dict["PatientID"])
+                    patient_class.store_info(information_dict)
+                    data_dict[information_dict["PatientID"]] = patient_class
+
+                elif pt_lb == "lab":
+                    lab_n = information_dict["LabName"]
+                    if lab_n not in data_dict:
+                        lab_class = Lab(lab_n)
+                        lab_class.store_info(information_dict)
+                        data_dict[lab_n] = lab_class
+                    else:
+                        data_dict[lab_n].store_info(information_dict)
+
+    return data_dict
